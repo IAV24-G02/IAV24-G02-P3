574da73f32efd1e54b88feeaf78a87d1996a9db9
theligero <iligero@ucm.es>
2024-04-25 00:11:15 +0200

    C. Herencia en Action y demás

--- Assets/Liquid Snake/Scripts/States/Action.cs
+++ Assets/Liquid Snake/Scripts/States/Action.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Action : MonoBehaviour
+public class Action
 {
+    public GameObject enemy;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public virtual void Start() { }
 
     // Update is called once per frame
-    public void Update()
+    public virtual void Update() { }
+}
+
+public class DoPatrol : Action
+{
+    private List<Transform> waypoints;
+
+    private Transform enemyTransform;
+    private Transform nextWaypoint;
+
+    private float enemySpeed = 5.0f;
+
+    public override void Start()
     {
-        
+        enemyTransform = enemy.GetComponent<Transform>();
+    }
+
+    public override void Update()
+    {
+        Vector3 dir = nextWaypoint.position - enemyTransform.position;
+
+        if (dir.magnitude > 1.0f)
+        {
+            dir.Normalize();
+
+            enemyTransform.position += dir * enemySpeed * Time.deltaTime;
+        }
+    }
+}
+
+public class RobotHunt : Action
+{
+    private GameObject player;
+
+    private Transform playerTransform;
+    private Transform enemyTransform;
+
+    private float enemySpeed = 5.0f;
+
+    public override void Start()
+    {
+        playerTransform = player.GetComponent<Transform>();
+        enemyTransform = enemy.GetComponent<Transform>();
+    }
+
+    public override void Update()
+    {
+        Vector3 dir = playerTransform.position - enemyTransform.position;
+
+        if (dir.magnitude > 1.0f)
+        {
+            dir.Normalize();
+
+            enemyTransform.position += dir * enemySpeed * Time.deltaTime;
+        }
     }
 }
